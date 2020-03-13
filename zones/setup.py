fed844cdf46eb30335b426819b43640519c314d8
Kwesi P Apponsah <kwesi@kwap-consulting.com>
2020-03-13 04:25:52 -0400

    add zones services from sample

---
+++ zones/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+setup(
+    name='mybnbaid-zones',
+    version='0.0.1',
+    description='Mybnbaid zones microservice',
+    packages=find_packages(exclude=['test', 'test.*']),
+    entry_points={
+        'console_scripts': ['mybnbaid.zones=zones.service:main']
+    },
+    install_requires=[
+        'rock==0.0.1',
+        'pyzmq==18.1.0',
+        'tornado==6.0.3'
+    ],
+    include_package_data=True,
+    extras_require={
+        'dev': [
+            'pytest==3.1.1',
+            'coverage==4.4.1',
+            'flake8==3.3.0'
+        ]
+    },
+    zip_safe=True
+)
